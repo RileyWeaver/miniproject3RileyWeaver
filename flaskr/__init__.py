7cbeedd52f69001525e85ead8f275b52e8ed1916
Riley Weaver <c0wman1928@live.com>
2025-03-22 02:59:02 -0400

    Added schema and db files as well as update __init__ file.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
 '''
 INF601 - Programming in Python
-Assignment #
+Assignment 3
 I,     Riley Weaver    , affirm that the work submitted for this assignment is entirely my own. I have not engaged in any form of academic dishonesty,
 including but not limited to cheating, plagiarism, or the use of unauthorized materials. I have neither provided nor received unauthorized assistance
 and have accurately cited all sources in adherence to academic standards. I understand that failing to comply with this integrity statement may result
@@ -40,5 +40,8 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
 
     return app
