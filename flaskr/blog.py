8bf94cc42ef8f1740597bdeb69ee635b1fb90bf4
Riley Weaver <c0wman1928@live.com>
2025-03-23 18:38:37 -0400

    Put in bootstrap and used a modal for post save button.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -12,7 +12,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, firstname'
+        'SELECT p.id, game, body, created, author_id, firstname'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -22,21 +22,21 @@ def index():
 @login_required
 def create():
     if request.method == 'POST':
-        title = request.form['title']
+        game = request.form['game']
         body = request.form['body']
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not game:
+            error = 'Game is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
+                'INSERT INTO post (game, body, author_id)'
                 ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                (game, body, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
@@ -45,7 +45,7 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, game, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -65,21 +65,21 @@ def update(id):
     post = get_post(id)
 
     if request.method == 'POST':
-        title = request.form['title']
+        game = request.form['game']
         body = request.form['body']
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not game:
+            error = 'Game is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
+                'UPDATE post SET game = ?, body = ?'
                 ' WHERE id = ?',
-                (title, body, id)
+                (game, body, id)
             )
             db.commit()
             return redirect(url_for('blog.index'))
